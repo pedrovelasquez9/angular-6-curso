c03424d9f481e4d4eb97b72e64237ce2f4cbcd86
Pedro Plasencia <pjpv9011@gmail.com>
2019-04-15 20:30:32 +0200

    animaciones

---
+++ Proyecto5/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AnimateComponent } from './animate/animate.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AnimateComponent
+  ],
+  imports: [
+    BrowserModule,
+	AppRoutingModule,
+	BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ proyecto6/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OpenCloseComponent } from './open-close/open-close.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OpenCloseComponent
+  ],
+  imports: [
+    BrowserModule,
+	AppRoutingModule,
+	BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
